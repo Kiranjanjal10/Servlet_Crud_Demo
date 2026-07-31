8b8c6f02de3695e4bcc0044c6300241292b623a6
Kiran janjal <kiranjanjal10@gmail.com>
2026-07-31 17:07:32 +0530

    Implement the doPost() and doGet() method

--- src/main/java/org/example/Service/UserService.java
+++ src/main/java/org/example/Service/UserService.java
@@ -2,7 +2,9 @@ package org.example.Service;
 
 import org.example.Model.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserService {
@@ -17,5 +19,20 @@ public class UserService {
     {
         userDb.put(userReq.getId(),userReq);
         return userReq;
+    }
+
+    public List<User> getAllUsers()
+    {
+        List<User> usersResp=new ArrayList<>();
+        for(User user:userDb.values())
+        {
+            usersResp.add(user);
+        }
+        return usersResp;
+    }
+
+    public User getUserById(Integer id)
+    {
+    return userDb.get(id);
     }
 }
--- src/main/java/org/example/Servlet/UserServlet.java
+++ src/main/java/org/example/Servlet/UserServlet.java
@@ -7,14 +7,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.example.Model.User;
 import org.example.Service.UserService;
 
-@WebServlet("/user")
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/users")
 public class UserServlet extends HttpServlet {
 
-    UserService service=new UserService();
+    UserService userService=new UserService();
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-    {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Integer id= Integer.parseInt(request.getParameter("id"));
         String name=request.getParameter("name");
         String email=request.getParameter("email");
@@ -23,28 +25,86 @@ public class UserServlet extends HttpServlet {
 
         if(id==null || name== null || email == null || mobile ==null)
         {
-
+            response.setStatus(400);
+            response.setContentType("application/json");
+            response.getWriter().write("{ \n"+
+                    " \"message\": \"Some fielda are missing\n"  +"}");
         }
         User user=new User(id,name,email,mobile);
 
-        User createduser=service.createuser(user);
+        User createduser=userService.createuser(user);
+
+        response.setStatus(201);
+        response.setContentType("application/json");
+        response.getWriter().write("{ \n"+
+                " \"message\": \"User added Successfully\n"  +"}");
+
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-    {
-        Integer id=Integer.parseInt(request.getParameter("id"));
-    }
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
 
+        String idParam = request.getParameter("id");
+
+        if(idParam == null) {
+            List<User> users = userService.getAllUsers();
+            response.setStatus(200);
+            response.setContentType("application/json");
+            response.getWriter().write(usersToJson(users));
+            return;
+        }
+        Integer id = Integer.parseInt(idParam);
+
+        User userResp = userService.getUserById(id);
+
+        if(userResp == null) {
+            response.setStatus(404);
+            response.setContentType("application/json");
+        }
+
+        response.setStatus(200);
+        response.setContentType("application/json");
+        response.getWriter().write(userToJson(userResp));
+
+    }
     @Override
     public void doPut(HttpServletRequest request, HttpServletResponse response)
     {
-        
+
     }
 
     @Override
     public void doDelete(HttpServletRequest request, HttpServletResponse response)
     {
 
+    }
+
+    private String userToJson(User userRes)
+    {
+        return "{\n" +
+                "    \"id\":"+userRes.getId()+",\n" +
+                "    \"name\":"+userRes.getName()+",\n" +
+                "    \"email\":"+userRes.getEmail()+",\n" +
+                "    \"mobile\":"+userRes.getMobile()+"\n" +
+                "}";
+    }
+
+    private String usersToJson(List<User> users)
+    {
+        StringBuilder builder=new StringBuilder();
+        builder.append("[");
+
+        for(int i=0;i<users.size();i++)
+        {
+            builder.append(userToJson(users.get(i)));
+
+            if(i<users.size()-1){
+                builder.append(",");
+            }
+
+        }
+        builder.append("]");
+        return builder.toString();
     }
 }
